24ef3cd957bcdb05759c76814bf32d15c0f0f5f5
Dharshan Bellan <dharshanb@saravanans-macbook-pro.local>
2018-10-15 16:14:08 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Dharshan Bellan on 10/15/18.
+//  Copyright © 2018 Dharshan Bellan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
