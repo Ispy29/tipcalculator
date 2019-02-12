e222e9ddd58e18d89bd72b6195f90c825f042a8d
Dharshan Bellan <dharshanb@Saravanans-MacBook-Pro.local>
2019-02-11 16:37:00 -0800

    first commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmt:
+        UITextField!
+    
+    
+    
+    
+    
+    @IBOutlet weak var tipPercentage:
+        UITextField!
+    
+    
+    
+    
+    
+    @IBOutlet weak var finalBill:
+        UILabel!
+    
+    
+    
+    
+    
+    var input1: Double? {
+        return Double(billAmt.text!)
+    }
+    
+    
+    
+    @IBAction func calculateBill(_ sender: Any) {
+        if let input1 = self.input1 as Double? {
+            print(input1)
+        }
+    }
+    
+    func calculateBill(billAmount: Double, tipPercentage: Int) -> Double {
+        let billTotal: Double = billAmount * (1.0 + (Double(tipPercentage) / 100))
+        return billTotal
+    }
+
+
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(calculateBill(billAmount: 30, tipPercentage: 20))
     }
 
     override func didReceiveMemoryWarning() {
